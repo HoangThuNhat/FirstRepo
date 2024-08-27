b2461311fffc7f2dbc2c02c16914d2ed2442ec78
HoangThuNhat <hoang04006@gmail.com>
2024-08-27 22:48:55 +0700

    sua thanh heloo, hehe

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         
-        System.out.println("Hello, Bao em!");
+        System.out.println("Hello, hehe!");
     }
 }
